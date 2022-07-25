1ec819056b96ebe28d81f3f69842f71d92afd48f
Vladislav <vldtpk@gmail.com>
2022-07-25 18:41:00 +0300

    FF

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();              //Instantiate an express app, the main work 
 const port = process.env.PORT || 5000;                  //Save the port number where your server will be listening
 
 //Idiomatic expression in express to route and respond to a client request
-app.use(express.static(__dirname + '/app'));
+app.use(express.static('app'));
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     res.sendFile('app/index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
